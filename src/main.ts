5b6a0dc201c4322adb1ed86e5afdbeb14a23c3bc
alhinator <alex@leghart.org>
2024-10-23 12:41:19 -0700

    removed vite demo & added basic phaser config

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,24 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import Phaser from "phaser"
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const CONFIG = {
+  type: Phaser.CANVAS,
+  parent: 'phaser-game',
+  scale: {
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    size: Phaser.Scale.FIT,
+    zoom: 0.75
+  },
+  pixelArt: true,
+  physics: {
+    default: 'arcade',
+    arcade: {
+    }
+  },
+  zoom: 1,
+  scene: [/*StartScene, GameplayScene*/]
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+export default CONFIG;
+
+const GAME = new Phaser.Game(CONFIG)
