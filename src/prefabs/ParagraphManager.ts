5d7b0c5c2670860eeed4020f14a920321a61e65d
joshwidjaja <josh@widjaja.us>
2024-10-27 20:58:31 -0700

    rearranged paragraph & wordCheck, still an error

--- src/prefabs/ParagraphManager.ts
+++ src/prefabs/ParagraphManager.ts
@@ -1,8 +1,29 @@
+interface WordCheck {
+    word: string,
+    isCompleted: boolean
+}
+
+class Paragraph {
+    paragraph: string;
+    words: WordCheck[];
+
+    constructor(paragraph: string) {
+        this.paragraph = paragraph;
+        this.words = [];
+
+        let wordList = paragraph.split(" ");
+        wordList.forEach(function (word: string) {
+            let newWordCheck: WordCheck = { word: word, isCompleted: false };
+            this.words.push(newWordCheck);
+        });
+    }
+}
+
 /**
 @class ParagraphManager a class for handling the paragraphs and word contents to be used in the main prototype.
  **/
 export class ParagraphManager {
-    paragraphs: string[];
+    paragraphs: Paragraph[];
 
     constructor() {
         this.paragraphs = [];
