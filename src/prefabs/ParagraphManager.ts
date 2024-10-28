bcc3e87b75e808a585f5d534e1c916979fac12ee
joshwidjaja <josh@widjaja.us>
2024-10-27 17:28:03 -0700

    moved paragraph manager

---
+++ src/prefabs/ParagraphManager.ts
@@ -0,0 +1,10 @@
+/**
+@class ParagraphManager a class for handling the paragraphs and word contents to be used in the main prototype.
+ **/
+export class ParagraphManager {
+    paragraphs: string[];
+
+    constructor() {
+        this.paragraphs = [];
+    }
+}
