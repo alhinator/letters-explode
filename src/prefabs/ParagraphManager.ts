cbdc8a2febf886a135ef2ff75f6b01e63ff4138a
joshwidjaja <josh@widjaja.us>
2024-10-29 11:50:22 -0700

    added LetterCheck

--- src/prefabs/ParagraphManager.ts
+++ src/prefabs/ParagraphManager.ts
@@ -1,11 +1,19 @@
+// import { Letter } from "./UIManager";
+
 interface WordCheck {
     word: string,
     isCompleted: boolean
 }
 
+interface LetterCheck<integer> {
+    [key: string]: integer;
+}
+
 class Paragraph {
     paragraph: string;
     words: WordCheck[];
+    length: integer;
+    letterCount: LetterCheck<integer>;
 
     constructor(paragraph: string) {
         this.paragraph = paragraph;
@@ -16,16 +24,23 @@ class Paragraph {
             let newWordCheck: WordCheck = { word: word, isCompleted: false };
             this.words.push(newWordCheck);
         });
+
+        this.length = this.words.length;
+
+        this.letterCount = {};
     }
 }
+
+type ParagraphSize = "SHORT" | "MEDIUM" | "LONG";
 
 /**
 @class ParagraphManager a class for handling the paragraphs and word contents to be used in the main prototype.
  **/
 export class ParagraphManager {
-    paragraphs: Paragraph[];
+    paragraph: Paragraph;
 
-    constructor() {
-        this.paragraphs = [];
+
+    constructor(which_one: ParagraphSize) {
+        this.paragraph = new Paragraph(""); // grabfromjson(which_one)
     }
 }
