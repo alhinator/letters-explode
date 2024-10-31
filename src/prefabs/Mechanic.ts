9f343374f27ae15b348f9a3a72917a96f8c1e69b
JackyPHO <100171161+JackyPHO@users.noreply.github.com>
2024-10-30 19:10:49 -0700

    randomLetter

--- src/prefabs/Mechanic.ts
+++ src/prefabs/Mechanic.ts
@@ -8,7 +8,7 @@ import { UserInput } from './UserInput';
  **/
 const markInterval: number = 15;
 const maxGameTime: number = 100;
-const alphabet: ["a", "b", "c", "d", "e", "f", "g", "h", "i",
+const alphabet: Letter[] = ["a", "b", "c", "d", "e", "f", "g", "h", "i",
     "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
     "t", "u", "v", "w", "x", "y", "z"];
 
